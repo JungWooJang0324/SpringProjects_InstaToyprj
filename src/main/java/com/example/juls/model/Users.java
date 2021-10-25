efc346b637807a7d244fd3b2d3e6a545a8267a87
Juliana0324 <62527384+Juliana0324@users.noreply.github.com>
2021-10-25 22:06:17 +0900

    Update Users.java

--- src/main/java/com/example/juls/model/Users.java
+++ src/main/java/com/example/juls/model/Users.java
@@ -3,7 +3,10 @@ package com.example.juls.model;
 import java.sql.Timestamp;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -14,12 +17,20 @@ import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@SequenceGenerator(
+		name="USER_SEQ_GEN",
+		sequenceName = "USER_SEQ",
+		initialValue=1,	//시작값
+		allocationSize=1	//메모리를 통한 할당할 범위사이즈
+		)
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class Users {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE,
+					generator="USER_SEQ_GEN")
 	private int id;
 	private String username;
 	private String password;
